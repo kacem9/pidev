db84bee8c4eafe7b2f3a36950e35989e9e599574
mohamed wassim gabtni <mohamedwassim.gabtni1@esprit.tn>
2020-02-24 22:14:39 +0100

    v1

---
+++ src/ProduitBundle/Controller/PanierController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace ProduitBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use ProduitBundle\Entity\Produit;
+
+class PanierController extends Controller
+{
+    public function removePanierAction(SessionInterface $session,$id,$nre)
+{
+    $em=$this->getDoctrine()->getManager();
+    $pt=$em->getRepository(Produit::class)->find($id);
+    $pt->setQte($pt->getQte()+($nre+0));
+    $em->persist($pt);
+    $em->flush();
+
+
+    $panier =$session->get('panier',[]);
+if(!empty($panier[$id])){
+    unset($panier[$id]);
+
+}
+$session->set('panier',$panier);
+
+return $this->redirectToRoute("panier");
+}
+
+    public function indexAction(SessionInterface $session)
+    {
+        $em=$this->getDoctrine();
+
+        $panier=$session->get('panier',[]);
+
+
+        $panierWithData=[];
+
+        foreach($panier as $id =>$qte){
+
+            $panierWithData[]=[
+                'produit' => $em->getRepository('ProduitBundle:Produit')->find($id),
+                'qte' => $qte
+            ];
+         }
+
+        $total =0;
+        foreach($panierWithData as $item){
+
+            $totalItem=$item['produit']->getPrix()* $item['qte'];
+           // var_dump($totalItem);
+            $total+=$totalItem;
+        }
+
+        return $this->render('produit/cart.html.twig',array('items' => $panierWithData, 'total' => $total));
+}
+
+
+    public function addPanierAction(SessionInterface $session,$id){
+
+
+$em=$this->getDoctrine()->getManager();
+$pt=$em->getRepository(Produit::class)->find($id);
+$pt->setQte($pt->getQte()-1);
+$em->persist($pt);
+$em->flush();
+
+       $panier= $session->get('panier',[]);
+
+    if(!empty($panier[$id])){
+            $panier[$id]++;
+        }
+        else{
+            $panier[$id]=1;
+        }
+        $session->set('panier',$panier);
+
+ return $this->redirectToRoute('panier');
+    }
+
+
+
+
+}
